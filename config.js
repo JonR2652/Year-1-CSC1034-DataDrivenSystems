7703b354d71ca3846078bc707ecf8393513612da
JonR2652 <jroberts2652@gmail.com>
2025-03-14 17:52:31 +0000

    db management

---
+++ config.js
@@ -0,0 +1,7 @@
+const dbConnectorUrl = 'https://jroberts15.webhosting1.eeecs.qub.ac.uk/dbConnector.php';
+let dbConfig = new URLSearchParams({
+    hostname: 'localhost',
+    username: 'jroberts15',
+    password: 'wG4mlN7TtsbrBPbm',
+    database: 'jroberts15',
+    });
